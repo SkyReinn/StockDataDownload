036a3f4f75b92a7622dd78a4567cfd89c6905b0b
Your Name <119990674+SkyReinn@users.noreply.github.com>
2023-08-07 14:30:07 -0400

    Update download.js

--- download.js
+++ download.js
@@ -1,61 +1,62 @@
+// Import libraries
 const { NseIndia } = require("stock-nse-india");
-const prompt = require("prompt");
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
+// Define stock object
 const nseIndia = new NseIndia();
 
-prompt.start();
+// Define stock symbol & start/end date
+const symbol = 'DIVISLAB';
+const startDate = new Date('2023-07-07');
+const endDate = new Date('2023-08-07');
 
-prompt.get(["symbol", "startDate", "endDate"], function (err, result) {
-    if (err) {
-        console.error(err);
-        return;
-    }
+// Define date range
+const range = {
+    start: startDate,
+    end: endDate,
+};
 
-    const symbol = result.symbol;
-    const startDate = new Date(result.startDate);
-    const endDate = new Date(result.endDate);
 
-    const range = {
-        start: startDate,
-        end: endDate,
-    };
+nseIndia
+    .getEquityHistoricalData(symbol, range)
+    .then((response) => {
+        // Acquire data from API
+        const data = response[0].data;
 
-    nseIndia
-        .getEquityHistoricalData(symbol, range)
-        .then((response) => {
-            const data = response[0].data;
+        // Print data
+        console.log(data);
 
-            console.log("Received Data:", data);
+        // Select & format data
+        const formattedData = data.map((item) => ({
+            Date: item.CH_TIMESTAMP,
+            Open: item.CH_OPENING_PRICE,
+            High: item.CH_TRADE_HIGH_PRICE,
+            Low: item.CH_TRADE_LOW_PRICE,
+            Close: item.CH_CLOSING_PRICE,
+        }));
 
-            const formattedData = data.map((item) => ({
-                Date: item.CH_TIMESTAMP,
-                Open: item.CH_OPENING_PRICE,
-                High: item.CH_TRADE_HIGH_PRICE,
-                Low: item.CH_TRADE_LOW_PRICE,
-                Close: item.CH_CLOSING_PRICE,
-            }));
+        // Define file name
+        const fileName = `${symbol}_${startDate.toISOString().slice(0, 10)}~${endDate.toISOString().slice(0, 10)}_historical_data.csv`;
 
-            const fileName = `${symbol}_${startDate.toISOString().slice(0, 10)}~${endDate.toISOString().slice(0, 10)}_historical_data.csv`;
+        // Create csv file
+        const csvWriter = createCsvWriter({
+            path: fileName,
+            header: [
+                { id: "Date", title: "Date" },
+                { id: "Open", title: "Open" },
+                { id: "High", title: "High" },
+                { id: "Low", title: "Low" },
+                { id: "Close", title: "Close" },
+            ],
+        });
 
-            const csvWriter = createCsvWriter({
-                path: fileName,
-                header: [
-                    { id: "Date", title: "Date" },
-                    { id: "Open", title: "Open" },
-                    { id: "High", title: "High" },
-                    { id: "Low", title: "Low" },
-                    { id: "Close", title: "Close" },
-                ],
+        // Write csv file
+        csvWriter
+            .writeRecords(formattedData)
+            .then(() => {
+                console.log("CSV file has been written successfully.");
+            })
+            .catch((csvError) => {
+                console.error("Error writing CSV file:", csvError);
             });
-
-            csvWriter
-                .writeRecords(formattedData)
-                .then(() => {
-                    console.log("CSV file has been written successfully.");
-                })
-                .catch((csvError) => {
-                    console.error("Error writing CSV file:", csvError);
-                });
-        })
-});
+    });
